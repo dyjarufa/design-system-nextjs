672d54b693b54a26eceaaab7dc6a3e0540e569b8
Jady Rufino <jadyrufa@gmail.com>
2020-09-17 10:50:55 -0300

    final structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-import { Heading, Grid, Flex } from '@chakra-ui/core'
+import { Heading, Grid, Flex, Link, Button, Text } from '@chakra-ui/core'
+import Divider from '../component/Divider'
+import Input from '../component/Input'
 
 
 export default function Home() {
   return (
     
     <Grid
-      as="main" //por pádrão o chakra seta uma div altero para uma tag main.
+      as="main" //por pádrão o chakra seta uma div, enão altero para uma tag main.
       height="100vh" //Quero que ocupe a altura inteira
       templateColumns="1fr 480px 480px 1fr" //1 coluna de tamanho flexível / Duas colunas de tamnho 480 / 1 Coluna de tamnaho flexivel
       templateRows="1fr 480px 1fr" //1 linha de tamnhao flexivel / uma linha tamnaho 480 / Outra linha tamnho flexivel
@@ -21,7 +23,7 @@ export default function Home() {
         alignItems="center"
       >
       
-      <Flex //Flex serio o display Grid
+      <Flex //Flex seria o display Grid
        gridArea="logo" flexDir="column" alignItems="flex-start">
         <img src="/rocketseat.svg" alt="Rocketseat"/>
 
@@ -32,10 +34,79 @@ export default function Home() {
            >
             Faça seu login na plataforma
           </Heading>
-      
       </Flex> 
 
+      <Flex
+        gridArea="form"
+        height="100%"
+        backgroundColor="gray.700"
+        borderRadius="md"
+        flexDir="column"
+        alignItems="stretch"
+        padding={16}
+      >
+        <Input 
+          placeholder="E-mail"
+        />
+        
+        <Input
+          marginTop={2} 
+          placeholder="Senha"
+        />
+        <Link
+          alignSelf="flex-start"
+          marginTop={2}
+          fontSize="sm"
+          color="purple.600"
+          fontWeight="bold"
+          _hover={{color: "purple.500"}}
+        >
+          Esqueci minha senha          
+        </Link>
+        <Button
+          backgroundColor="purple.500"
+          height="50px"
+          borderRadius="sm"
+          marginTop={6}
+          _hover={{backgroundColor: "purple.600"}}  
+        >
+          ENTRAR
+        </Button>
+
+        <Text
+          textAlign="center"
+          fontSize="sm"
+          marginTop={6}
+          color="gray.300"
+        >
+          Não tem uma conta? {" "}
+
+          <Link
+            color="purple.600"
+            fontWeight="bold"
+            _hover={{ color: "purple.500" }}
+          >
+          registre-se
+          </Link>
+        </Text>
+
+        <Divider />
       
+        <Flex alignItems="center">
+          <Text fontSize="sm"> Ou entre com</Text>
+          <Button
+            height="50px"
+            flex="1"
+            backgroundColor="gray.600"
+            borderRadius="sm"
+            marginLeft={6}
+            _hover={{ backgroundColor: 'purple.500'}}
+          >
+          GITHUB
+          </Button>
+        </Flex>
+      </Flex>
+
       
     </Grid>
   )
--- styles/theme.ts
+++ styles/theme.ts
@@ -7,23 +7,27 @@ const customTheme: DefaultTheme = {
     heading: 'Roboto, system-ui, sans-serif',
     mono: 'Menlo, monospace'
   },
+  
   fontWeights: {
     ...theme.fontWeights,
     normal: 400,
     medium: 600,
     bold: 700,
   },
+
   radii: {
     ...theme.radii,
     sm: '5px',
     md: '8px',
   },
+
   colors: {
     ...theme.colors,
     purple: {
       ...theme.colors.purple,
       500: '#8257e5',
     },
+
     gray: {
       ...theme.colors.gray,
       300: '#e1e1e6',
